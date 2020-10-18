d03f3925aab35db7b968878e4fb652800bbfcb2f
mk668a <msftoaa@gmail.com>
2020-10-18 20:10:35 +0900

    自動送信message追加

--- main.py
+++ main.py
@@ -64,5 +64,7 @@ if __name__ == "__main__":
         "おはよう\u1F600",
         "\uDBC0\uDC78"
     ]
+    line_bot_api.push_message("aaotfsm", messages=TextSendMessage(
+        text=texts[random.randint(0, len(texts)-1)]))
     line_bot_api.broadcast(TextSendMessage(
         text=texts[random.randint(0, len(texts)-1)]))
